fbf80e4fd5790af32ab88623470d7be969d96aee
engincanaraz <engincanaraz@outlook.com>
2025-03-10 07:57:37 +0300

    feat:pwa desteği eklendi

---
+++ public/icons/README.md
@@ -0,0 +1,19 @@
+# PWA Icons
+
+This directory should contain the following icon files for the Progressive Web App:
+
+- icon-72x72.png (72x72 pixels)
+- icon-96x96.png (96x96 pixels)
+- icon-128x128.png (128x128 pixels)
+- icon-144x144.png (144x144 pixels)
+- icon-152x152.png (152x152 pixels)
+- icon-192x192.png (192x192 pixels)
+- icon-384x384.png (384x384 pixels)
+- icon-512x512.png (512x512 pixels)
+
+You can generate these icons from your existing ramadan.ico file using an online tool like:
+- [PWA Asset Generator](https://github.com/onderceylan/pwa-asset-generator)
+- [Favicon Generator](https://realfavicongenerator.net/)
+- [App Icon Generator](https://appicon.co/)
+
+After generating the icons, place them in this directory to complete your PWA setup.
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // Vite yapılandırması için: https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['src/ramadan.ico'],
+      manifest: {
+        name: 'İftara Ne Kadar Kaldı',
+        short_name: 'İftar Vakti',
+        description: 'Ramazan ayı boyunca iftar ve sahur vakitlerine geri sayım',
+        theme_color: '#4F46E5',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
